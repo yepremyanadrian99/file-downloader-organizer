b31a75485308cf32f7bd2841c92cd4b4a984e3d4
Yepremyan <adrian.yepremyan@picsart.com>
2022-12-16 20:28:36 +0400

    initial commit.

---
+++ src/main/java/am/adrianyepremyan/filedownloaderorganizer/FileDownloaderOrganizerApplication.java
@@ -0,0 +1,12 @@
+package am.adrianyepremyan.filedownloaderorganizer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FileDownloaderOrganizerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FileDownloaderOrganizerApplication.class, args);
+    }
+}
---
+++ src/test/java/am/adrianyepremyan/filedownloaderorganizer/FileDownloaderOrganizerApplicationTests.java
@@ -0,0 +1,12 @@
+package am.adrianyepremyan.filedownloaderorganizer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FileDownloaderOrganizerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+}
